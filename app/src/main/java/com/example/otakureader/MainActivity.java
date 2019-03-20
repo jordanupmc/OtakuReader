f373e3d8784c9c541d1056e0144537f2809be2dc
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2019-03-20 16:14:04 +0100

    Amélioration reader_chapter : -Passage aux com.github.chrisbanes.photoview.PhotoView pour les fonctionnalités de zoom sur les imageview -Passage a glide pour les images sur le réseau

--- app/src/main/java/com/example/otakureader/MainActivity.java
+++ app/src/main/java/com/example/otakureader/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.otakureader;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button read = findViewById(R.id.btn_go_chapter_view);
+        final Button read = findViewById(R.id.btn_go_chapter_view);
 
-        read.setOnClickListener(l->{
-            Intent intent = new Intent(this, FullscreenView.class);
+        read.setOnClickListener(l -> {
+            final Intent intent = new Intent(this, FullscreenView.class);
             startActivity(intent);
         });
     }
